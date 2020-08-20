608a9ed689fc669a4eb00e9773d515b7a5431821
Yue Su <34404957+yue-su@users.noreply.github.com>
2020-08-20 18:17:15 -0300

    setup index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-import ProTip from '../components/ProTip';
-import Link from '../components/Link';
-import Copyright from '../components/Copyright';
+import React from "react"
+import Container from "@material-ui/core/Container"
+import Typography from "@material-ui/core/Typography"
+import Box from "@material-ui/core/Box"
+import Link from "../components/Link"
+import { Paper, Grid } from "@material-ui/core"
+import { makeStyles } from "@material-ui/core/styles"
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    width: '612px',
+    height: '792px',
+    display: 'grid',
+    placeItems: 'center'
+  },
+}))
 
 export default function Index() {
+
+  const classes = useStyles()
+
   return (
-    <Container maxWidth="sm">
-      <Box my={4}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Gatsby v4-beta example
-        </Typography>
-        <Link to="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
-        <Copyright />
-      </Box>
-    </Container>
-  );
+    <Box align="center">
+      <Paper square className={classes.paper}>
+        <Box
+          width="600px"
+          height="780px"
+          border="3px solid"
+          borderRadius="10px"
+        >
+          <Grid container>
+            <Grid item sm={4}>test</Grid>
+            <Grid item sm={8}>test</Grid>
+          </Grid>
+        </Box>
+      </Paper>{" "}
+    </Box>
+  )
 }
